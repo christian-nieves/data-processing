01e2df07c4ca8eb3b0ae36ef207b45dc39d33fc8
christian-nieves <thechristiannieves@gmail.com>
2026-05-19 10:37:57 -0400

    Done with Person class

--- src/main/java/com/pluralsight/loops/Program.java
+++ src/main/java/com/pluralsight/loops/Program.java
@@ -2,12 +2,35 @@ package com.pluralsight.loops;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
         List<Person> people = new ArrayList<>();
 
-        people.add(new Person("John", "Doe", 21))
+        people.add(new Person("John", "Doe", 21));
+        people.add(new Person("Jimmy", "John", 23));
+        people.add(new Person("Bobby", "Lee", 19));
+        people.add(new Person("Christian", "Nieves", 18));
+        people.add(new Person("Mark", "Williams", 20));
+        people.add(new Person("Daniel",  "Martinez", 67));
+        people.add(new Person("Emily",   "Davis",    24));
+        people.add(new Person("Ryan",    "Wilson",   41));
+        people.add(new Person("Ashley",  "Taylor",   29));
+
+        System.out.println("Search by Name: ");
+        String userInput = scanner.nextLine();
+
+        List<Person> foundNames = new ArrayList<>();
+
+        for (Person person : people) {
+            if (person.getFirstName().equalsIgnoreCase(userInput) || person.getLastName().equalsIgnoreCase(userInput))
+                foundNames.add(person);
+        }
+
+        for (Person foundName : foundNames) {
+            System.out.println(foundName);
+        }
     }
 }
