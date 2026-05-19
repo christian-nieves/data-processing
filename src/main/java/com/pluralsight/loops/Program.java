5ef7a1d79b2afbf4f4d5b658f0331295a897e297
christian-nieves <thechristiannieves@gmail.com>
2026-05-19 10:22:26 -0400

    Added construsctor in Person class

--- src/main/java/com/pluralsight/loops/Program.java
+++ src/main/java/com/pluralsight/loops/Program.java
@@ -1,4 +1,13 @@
 package com.pluralsight.loops;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Program {
+    public static void main(String[] args) {
+
+        List<Person> people = new ArrayList<>();
+
+        people.add(new Person("John", "Doe", 21))
+    }
 }
